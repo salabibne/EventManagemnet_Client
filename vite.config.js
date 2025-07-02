e0660e8e9f298f4a43f4b4a7a8c62dab3c54330f
salabibne <salabyaham@gmail.com>
2025-07-02 20:23:26 +0600

    build two

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.'
-        }
-      ]
-    })
   ],
   build: {
     outDir: 'dist',
