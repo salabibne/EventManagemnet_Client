c8775f0cb3db6c81ff5373e68e7a2f06f1c6e3df
salabibne <salabyaham@gmail.com>
2025-07-02 19:53:15 +0600

    build

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,8 @@ import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  // Wrapping the App component with BrowserRouter and AuthProvider
+  <BrowserRouter> 
     <AuthProvider>
       <App />
     </AuthProvider>
