e5fefab47276d7bcac04600dfcbc70dcadd780f4
salabibne <salabyaham@gmail.com>
2025-07-02 19:39:05 +0600

    focefully add redirects

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.'
+        }
+      ]
+    })
+  ],
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
   },
-  server: {
-    historyApiFallback: true
-  }
 })
