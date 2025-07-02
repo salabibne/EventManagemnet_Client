eafb40f4951cc67b9b8f03556febfa44821372e9
salabibne <salabyaham@gmail.com>
2025-07-02 19:49:46 +0600

    main and app file configures to browser path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import AppRoutes from "./routes/AppRoutes";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+//       <AppRoutes />
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import Navbar from "./components/Navbar";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <AppRoutes />
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { AuthProvider } from "./context/AuthContext";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+ 
+//     <AuthProvider>
+//       <App />
+//     </AuthProvider>
+  
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- 
+  <BrowserRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  
+  </BrowserRouter>
 );
